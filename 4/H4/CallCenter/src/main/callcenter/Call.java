ba366d529044fc0a9adc8fe9e413e09f60d2c1d1
Danila A. Koryakin <dkoryakin@tsft.ru>
2019-03-25 22:28:03 +0300

    third task

---
+++ 4/H4/CallCenter/src/main/callcenter/Call.java
@@ -0,0 +1,21 @@
+package callcenter;
+
+public class Call {
+
+    private long begin;
+
+    private long end;
+
+    public long getBegin() {
+        return begin;
+    }
+
+    public long getEnd() {
+        return end;
+    }
+
+    public Call(long begin, long end) {
+        this.begin = begin;
+        this.end = end;
+    }
+}
